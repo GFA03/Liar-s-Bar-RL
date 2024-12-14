97bbf10b146bf2fc7577b4a9e772e665b891d4b1
GFA03 <florinalexandru302003@gmail.com>
2024-12-14 17:32:09 +0200

    refactor: move decision-making inside the player class

--- main.py
+++ main.py
@@ -4,44 +4,7 @@ from typing import List
 from card import Card
 from game import InvalidChallengeError, InvalidTurnError, LiarsBarEnv
 from player import Player
-
-def read_int(statement: str = '') -> int:
-  while True:
-    try:
-      return int(input(statement))
-    except ValueError:
-      print("Invalid input, try again")
-
-def choose_action(current_player: Player) -> tuple[int, List[Card]]:
-  chosen_option = -1
-  while chosen_option not in [1, 2]:
-    chosen_option = read_int("Choose option\n1 - Play hand\n2 - Challenge previous player\n")
-  if chosen_option == 1:
-    return (1, choose_cards(current_player))
-  return (2, [])
-
-def choose_cards(current_player: Player) -> List[Card]:
-  chosen_cards = []
-  display_hand(current_player)
-  while True:
-    print("Chosen cards:\n", *chosen_cards)
-    chosen_card = read_int("Type the number of the card you want to play, or -1 to stop\n")
-    if chosen_card == -1:
-      break
-    if 0 <= chosen_card < len(current_player.hand):
-      toggle_card_selection(current_player, chosen_cards, chosen_card)
-  return chosen_cards
-
-def display_hand(player: Player) -> None:
-  for i, card in enumerate(player.hand):
-    print(f"{i} - {card}")
-
-def toggle_card_selection(current_player: Player, chosen_cards: List[Card], chosen_card: int) -> None:
-  card = current_player.hand[chosen_card]
-  if card in chosen_cards:
-      chosen_cards.remove(card)
-  else:
-      chosen_cards.append(card)
+from utils import read_int
 
 def initialize_game() -> LiarsBarEnv:
   game = LiarsBarEnv()
@@ -57,7 +20,10 @@ def play_round(game: LiarsBarEnv) -> None:
     print("Current table card:", game.table_card)
     execute_player_action(game)
     time.sleep(2)
-    os.system('cls' if os.name == 'nt' else 'clear')
+    clear_screen()
+
+def clear_screen() -> None:
+  os.system('cls' if os.name == 'nt' else 'clear')
 
 def display_round_status(game: LiarsBarEnv) -> None:
   print('Initializing round!')
@@ -68,7 +34,7 @@ def display_round_status(game: LiarsBarEnv) -> None:
 
 def execute_player_action(game: LiarsBarEnv) -> None:
   while True:
-    chosen_option, chosen_cards = choose_action(game.get_current_player())
+    chosen_option, chosen_cards = game.get_current_player().choose_action()
     try:
       if chosen_option == 1:
         game.play_turn(chosen_cards)
--- player.py
+++ player.py
@@ -1,7 +1,46 @@
+from card import Card
+from utils import read_int
+
 class Player:
   def __init__(self, name: str) -> None:
     self.name = name
     self.hand = []
     self.bullets_shot = 0
     self.death_bullet = 0
+
+  def take_shot(self) -> None:
+    self.bullets_shot += 1
+    return self.bullets_shot == self.death_bullet
   
+  def choose_action(self) -> tuple[int, list[Card]]:
+    chosen_option = -1
+    while chosen_option not in [1, 2]:
+      chosen_option = read_int(f"{self.name}, choose option\n1 - Play hand\n2 - Challenge previous player\n")
+    if chosen_option == 1:
+      return (1, self.choose_cards())
+    return (2, [])
+  
+  def choose_cards(self) -> list[Card]:
+    chosen_cards = []
+    self.display_hand()
+    while True:
+      print("Chosen cards:\n", *chosen_cards)
+      chosen_card = read_int("Type the number of the card you want to play, or -1 to stop\n")
+      if chosen_card == -1:
+        break
+      if 0 <= chosen_card < len(self.hand):
+        self.toggle_card_selection(chosen_cards, chosen_card)
+      else:
+        print("Invalid card number, try again")
+    return chosen_cards
+  
+  def display_hand(self) -> None:
+    for i, card in enumerate(self.hand):
+      print(f"{i} - {card}")
+
+  def toggle_card_selection(self, chosen_cards: list[Card], chosen_card: int) -> None:
+    card = self.hand[chosen_card]
+    if card in chosen_cards:
+        chosen_cards.remove(card)
+    else:
+        chosen_cards.append(card)
