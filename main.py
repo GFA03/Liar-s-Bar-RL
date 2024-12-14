0a67452087777a739e30433d8f685f5b621fbe1d
GFA03 <florinalexandru302003@gmail.com>
2024-12-14 13:26:41 +0200

    refactor: clean main code

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import os
 import time
 from typing import List
 from card import Card
-from game import LiarsBarEnv
+from game import InvalidChallengeError, InvalidTurnError, LiarsBarEnv
 from player import Player
 
 def read_int(statement: str = '') -> int:
@@ -11,55 +12,75 @@ def read_int(statement: str = '') -> int:
     except ValueError:
       print("Invalid input, try again")
 
-'''
-Returns the chosen option, and a list of cards for the first option
-'''
 def choose_action(current_player: Player) -> tuple[int, List[Card]]:
   chosen_option = -1
-  while chosen_option != 1 and chosen_option != 2:
+  while chosen_option not in [1, 2]:
     chosen_option = read_int("Choose option\n1 - Play hand\n2 - Challenge previous player\n")
   if chosen_option == 1:
-    chosen_cards = []
-    while len(chosen_cards) == 0 or len(chosen_cards) > 3:
-      print("Choose cards to play (up to 3)")
-      chosen_cards = choose_cards(current_player)
-    return (1, chosen_cards)
+    return (1, choose_cards(current_player))
   return (2, [])
 
 def choose_cards(current_player: Player) -> List[Card]:
   chosen_cards = []
-  for i, card in enumerate(current_player.hand):
-    print(f"{i} - {card}")
-  print("Type the number of the card you want to play, or -1 to stop")
+  display_hand(current_player)
   while True:
     print("Chosen cards:\n", *chosen_cards)
-    chosen_card = -2
-    while chosen_card < -1 or chosen_card >= len(current_player.hand):
-      chosen_card = read_int()
+    chosen_card = read_int("Type the number of the card you want to play, or -1 to stop\n")
     if chosen_card == -1:
       break
-    if current_player.hand[chosen_card] in chosen_cards:
-      chosen_cards.remove(current_player.hand[chosen_card])
-    else:
-      chosen_cards.append(current_player.hand[chosen_card])
+    if 0 <= chosen_card < len(current_player.hand):
+      toggle_card_selection(current_player, chosen_cards, chosen_card)
   return chosen_cards
 
-if __name__ == '__main__':
+def display_hand(player: Player) -> None:
+  for i, card in enumerate(player.hand):
+    print(f"{i} - {card}")
+
+def toggle_card_selection(current_player: Player, chosen_cards: List[Card], chosen_card: int) -> None:
+  card = current_player.hand[chosen_card]
+  if card in chosen_cards:
+      chosen_cards.remove(card)
+  else:
+      chosen_cards.append(card)
+
+def initialize_game() -> LiarsBarEnv:
   game = LiarsBarEnv()
   game.add_player(Player("Alex"))
   game.add_player(Player("Edi"))
-  while len(game.players) > 1:
-    print("Initializing round!")
-    game.initialize_round()
-    while not game.round_finished:
-      print(f"Current player: {game.get_current_player().name}")
-      print("Current table card:", game.table_card)
-      option_status = False
-      while option_status is False:
-        chosen_option, chosen_cards = choose_action(game.get_current_player())
-        if chosen_option == 1:
-          option_status = game.play_turn(chosen_cards)
-        else:
-          option_status = game.challenge_last_player()
-        time.sleep(1)
+  return game
+
+def play_round(game: LiarsBarEnv) -> None:
+  display_round_status(game)
+  game.initialize_round()
+  while not game.round_finished:
+    print(f"Current player: {game.get_current_player().name}")
+    print("Current table card:", game.table_card)
+    execute_player_action(game)
     time.sleep(2)
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def display_round_status(game: LiarsBarEnv) -> None:
+  print('Initializing round!')
+  print('Players:')
+  for player in game.players:
+    print(f'{player.name} - {player.bullets_shot}/{game.MAX_DEATH_BULLET} shots')
+  print()
+
+def execute_player_action(game: LiarsBarEnv) -> None:
+  while True:
+    chosen_option, chosen_cards = choose_action(game.get_current_player())
+    try:
+      if chosen_option == 1:
+        game.play_turn(chosen_cards)
+      else:
+        game.challenge_last_player()
+      break
+    except (InvalidTurnError, InvalidChallengeError) as e:
+      print(e)
+    time.sleep(1)
+
+if __name__ == '__main__':
+  game = initialize_game()
+  while len(game.players) > 1:
+    play_round(game)
+    time.sleep(1)
