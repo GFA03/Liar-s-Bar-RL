2aa6fdb85121b23cfbf70db843915a45ad9d8a74
GFA03 <florinalexandru302003@gmail.com>
2024-12-16 13:27:09 +0200

    feat: add random player

---
+++ random_player.py
@@ -0,0 +1,22 @@
+import random
+from typing import List
+from card import Card
+from player import Player
+
+class RandomPlayer(Player):
+  def choose_action(self) -> tuple[int, List[Card]]:
+      action = random.choice([1, 2])
+      if action == 1:
+          return (1, self.choose_cards())
+      return (2, [])
+  
+  def choose_cards(self) -> List[Card]:
+      chosen_cards = []
+      possible_cards = self.hand.copy()
+      num_of_cards = random.randint(1, 3)
+      for _ in range(min(num_of_cards, len(possible_cards))):
+          chosen_card = random.choice(possible_cards)
+          possible_cards.remove(chosen_card)
+          chosen_cards.append(chosen_card)
+      print(f"{self.name} chose cards: ", *chosen_cards)
+      return chosen_cards
