1627a0d4dbc14841a3b0a936c9fc7316c4945a7f
GFA03 <florinalexandru302003@gmail.com>
2025-01-15 12:51:40 +0200

    fixed qagent to work with new environment

--- qagent.py
+++ qagent.py
@@ -14,13 +14,13 @@ class QLearningAgent:
         # Initialize Q-table
         self.q_table = defaultdict(lambda: {})
 
-    def get_action(self, state):
+    def choose_action(self, state):
         available_actions = self.env._get_available_actions()
         if random.random() < self.exploration_rate:
             return random.choice(available_actions)
         else:
             state_key = self._state_to_key(state)
-        return max(available_actions, key=lambda action: self.q_table[state_key].get(action, 0.0))
+            return max(available_actions, key=lambda action: self.q_table[state_key].get(tuple(action), 0.0))
 
 
     def learn(self, state, action, reward, next_state, done):
@@ -28,16 +28,16 @@ class QLearningAgent:
         state_key = self._state_to_key(state)
         next_state_key = self._state_to_key(next_state)
 
-        print(action)
+        action_key = tuple(action)
         
         # Initialize Q-values for the state-action pair if not already done
-        if action not in self.q_table[state_key]:
-            self.q_table[state_key][action] = 0.0
+        if action_key not in self.q_table[state_key]:
+            self.q_table[state_key][action_key] = 0.0
 
         # Compute TD target and update rule
         best_next_action = max(self.q_table[next_state_key], key=self.q_table[next_state_key].get, default=None)
         td_target = reward + (self.discount_factor * self.q_table[next_state_key].get(best_next_action, 0.0) * (not done))
-        self.q_table[state_key][action] += self.learning_rate * (td_target - self.q_table[state_key][action])
+        self.q_table[state_key][action_key] += self.learning_rate * (td_target - self.q_table[state_key][action_key])
 
         if done:
             self.exploration_rate *= self.exploration_decay
@@ -46,5 +46,5 @@ class QLearningAgent:
         """Convert state dictionary to a tuple key for Q-table."""
         hand_key = tuple(state["hand"])
         table_card_key = state["table_card"]
-        last_played_key = state["last_played"]
-        return (hand_key, table_card_key, last_played_key)
+        history_key = tuple(state["history"])
+        return (hand_key, table_card_key, history_key)
